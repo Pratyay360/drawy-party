d85f5d324a8782185f8b7067192a0c8d3460ff03
pratyay mustafi <pratyaymustafi@outlook.com>
2026-08-29 22:31:11 +0530

    partykit

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -1,3 +1,7 @@
-export { calculateAssetsSize, compressDataUrl, optimizeBinaryFiles } from "./assets/compression";
+export {
+	calculateAssetsSize,
+	compressDataUrl,
+	optimizeBinaryFiles,
+} from "./assets/compression";
 export { pruneUnusedFiles } from "./assets/pruning";
 export { uploadPendingAssets } from "./assets/upload";
